014fd27e63a93fb66f8feb0c53c4c73c182f6de7
RubenSilva19 <rubenfilipe.ms@gmail.com>
2026-03-11 17:13:10 +0000

    Vite Config
    Remove dist , fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ command, mode }) => {
-  // Load all .env files
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    plugins: [react(),tailwindcss(),],
-    define: {
-      // Make process.env available globally
-      'process.env': env
-    }
-  }
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/'  // Important for Render
 })
 
 
